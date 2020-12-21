fcd7fddfdf292abccb2da8676dd034801e2b7abc
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2020-12-21 21:21:37 +0100

    initial

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import os
 from neo4j import GraphDatabase
+from config import database_password, database_user, database_uri, secret_key
 
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = secret_key
 
-driver = GraphDatabase.driver(uri="bolt://149.156.109.37:7687", auth=("u7markiewicz", "291630"))
+driver = GraphDatabase.driver(uri=database_uri, auth=(database_user, database_password))
 session = driver.session()
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, session
 from app import app
 from flask import Flask, render_template
-
+import os
 
 @app.route('/')
 def index():
+    print(os.environ.get("DATABASE_USER"))
     return render_template("_base.html")
     
 
