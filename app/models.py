d35e50a0b45d742e18ddac260ef8dc029806efc6
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2020-12-22 00:09:13 +0100

    added model and view

--- app/models.py
+++ app/models.py
@@ -1,26 +1,26 @@
 from py2neo import Graph, Node, Relationship
+from py2neo.matching import NodeMatcher
 from passlib.hash import bcrypt
 from datetime import datetime
 import os
+from config import database_uri, database_user, database_password
 import uuid
 
-url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
-username = os.environ.get('NEO4J_USERNAME')
-password = os.environ.get('NEO4J_PASSWORD')
-
-graph = Graph(url + '/db/data/', username=username, password=password)
+graph = Graph(database_uri, auth=(database_user, database_password))
 
 class User:
     def __init__(self, username):
         self.username = username
 
     def find(self):
-        user = graph.find_one('User', 'username', self.username)
+        matcher = NodeMatcher(graph)
+        user = matcher.match('User', username= self.username).first()
         return user
 
     def register(self, password):
         if not self.find():
             user = Node('User', username=self.username, password=bcrypt.encrypt(password))
+            print(user)
             graph.create(user)
             return True
         else:
@@ -34,7 +34,8 @@ class User:
             return False
 
     def add_post(self, title, tags, text):
-        user = self.find()
+        matcher = NodeMatcher(graph)
+        user = matcher.match('User', username=self.username).first()
         post = Node(
             'Post',
             id=str(uuid.uuid4()),
@@ -49,6 +50,8 @@ class User:
         tags = [x.strip() for x in tags.lower().split(',')]
         for name in set(tags):
             tag = Node('Tag', name=name)
+            tag.__primarylabel__ = "Tag"
+            tag.__primarykey__ = "name"
             graph.merge(tag)
 
             rel = Relationship(tag, 'TAGGED', post)
@@ -62,7 +65,7 @@ class User:
     def get_recent_posts(self):
         query = '''
         MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
-        WHERE user.username = {username}
+        WHERE user.username = $username
         RETURN post, COLLECT(tag.name) AS tags
         ORDER BY post.timestamp DESC LIMIT 5
         '''
@@ -75,7 +78,7 @@ class User:
         query = '''
         MATCH (you:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
               (they:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
-        WHERE you.username = {username} AND you <> they
+        WHERE you.username = $username AND you <> they
         WITH they, COLLECT(DISTINCT tag.name) AS tags
         ORDER BY SIZE(tags) DESC LIMIT 3
         RETURN they.username AS similar_user, tags
@@ -87,8 +90,8 @@ class User:
         # Find how many of the logged-in user's posts the other user
         # has liked and which tags they've both blogged about.
         query = '''
-        MATCH (they:User {username: {they} })
-        MATCH (you:User {username: {you} })
+        MATCH (they:User $username: $they )
+        MATCH (you:User $username: $you )
         OPTIONAL MATCH (they)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
                        (you)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
         RETURN SIZE((they)-[:LIKED]->(:Post)<-[:PUBLISHED]-(you)) AS likes,
@@ -100,7 +103,7 @@ class User:
 def get_todays_recent_posts():
     query = '''
     MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
-    WHERE post.date = {today}
+    WHERE post.date = $today
     RETURN user.username AS username, post, COLLECT(tag.name) AS tags
     ORDER BY post.timestamp DESC LIMIT 5
     '''
