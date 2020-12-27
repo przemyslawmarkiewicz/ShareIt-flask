a743770bb44c98d00ec4e7f95cb0a4286e0bc6bd
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2020-12-27 17:46:04 +0100

    added dependencies to requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 from neo4j import GraphDatabase
-from config import database_password, database_user, database_uri, secret_key
+# from config import database_password, database_user, database_uri, secret_key
 
 
 app = Flask(__name__)
