15310e06b7ab066f4bfb00764d818f12b14b5438
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2021-01-05 00:49:35 +0100

    heroku5

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ import os
 
 if __name__ == '__main__':
 
-    port = int(os.environ.get('PORT', 5000))
     app.secret_key = os.urandom(24)
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
