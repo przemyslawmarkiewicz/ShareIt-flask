874a0fa2314e836fae45bc3e455cf5655cb458b4
przemyslawmarkiewicz <przemyslawmarkiewicz97@gmail.com>
2021-01-05 01:19:22 +0100

    added session

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_session import Session
 import os
 from neo4j import GraphDatabase
 
+
 app = Flask(__name__)
+
+sess = Session()
+sess.init_app(app)
 
 from app import views
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from app import app
+from app import app, sess
 import os
 
 app.secret_key = os.urandom(24)
 app.config['SESSION_TYPE'] = 'filesystem'
+sess.init_app(app)
 
 if __name__ == '__main__':
     
